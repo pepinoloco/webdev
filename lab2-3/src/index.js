744b0a4f01eea0fb86978e3f043f896538dce19d
pepinoloco <pepinoloco@users.noreply.github.com>
2024-10-15 15:54:29 -0400

    lab2-1 publish

--- lab2-3/src/index.js
+++ lab2-3/src/index.js
@@ -1,16 +1,10 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client';
 
-function getTime() {
-  const now = new Date();
-  return now.toLocaleTimeString('en-US');
-}
-
 const App = () => {
   return (
     <div>
-      <div>Current Time:</div>
-      <h3>{ getTime() }</h3>
+       <p>Assignment</p>
     </div>
   );
 }
